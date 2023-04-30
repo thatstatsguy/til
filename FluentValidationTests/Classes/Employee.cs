b800ebb97526c4268d002cf323135b0142cd54d0
ThatStatsGuy <chris.dunderdale.1995@gmail.com>
2023-04-30 15:44:24 +0200

    tweak class validation rules

--- FluentValidationTests/Classes/Employee.cs
+++ FluentValidationTests/Classes/Employee.cs
@@ -4,10 +4,11 @@ namespace FluentValidationTests.Classes;
 
 public class Employee
 {
-    [Required]
+    [Required(ErrorMessage = "Name is required. It cannot be empty")]
     [MaxLength(50, ErrorMessage = "The maximum name length can only be 50 characters long")]
     public string? Name { get; set; }
 
-    [Required(ErrorMessage = "Surname is Required. It cannot be empty")]
-    public string Surname { get; set; } = "";
+    [Required(ErrorMessage = "Surname is required. It cannot be empty")]
+    [MaxLength(50, ErrorMessage = "The maximum surname length can only be 50 characters long")]
+    public string? Surname { get; set; }
 }
