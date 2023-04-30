eff5800011195aba7d5cda1b524c7e8eeb79f997
ThatStatsGuy <chris.dunderdale.1995@gmail.com>
2023-04-30 14:56:16 +0200

    Fluent Validation

--- FluentValidationTests/Classes/Manager.cs
+++ FluentValidationTests/Classes/Manager.cs
@@ -1,7 +1,40 @@
+using FluentValidation;
+
 namespace FluentValidationTests.Classes;
 
 public class Manager
 {
-    public string Name { get; set; }
-    public string Surname { get; set; }
+    public string Name { get; set; } = "";
+    public string Surname { get; set; } = "";
+    
+    public int Age { get; set; }
+}
+
+public class ManagerValidator : AbstractValidator<Manager>
+{
+    public ManagerValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("Manager name needs to be specified")
+            .MaximumLength(50).WithMessage("Manager name cannot be longer than 50 characters");
+
+        RuleFor(p => p.Surname)
+            .NotEmpty().WithMessage("Manager surname needs to be specified")
+            .MaximumLength(50).WithMessage("Last name cannot be longer than 50 characters");
+        
+        RuleFor(p => p.Age)
+            .NotNull()
+            .GreaterThan(0)
+            .Must(IsAgeValid).WithMessage("Age is too high!");
+    }
+    
+    /// <summary>
+    /// Custom logic validation
+    /// </summary>
+    /// <param name="age"></param>
+    /// <returns></returns>
+    private static bool IsAgeValid(int age)
+    {
+        return age < 100;
+    }
 }
