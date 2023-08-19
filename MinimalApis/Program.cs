b9a89482b5af67238ebe9c195463eb4bad2d1866
ThatStatsGuy <chris.dunderdale.1995@gmail.com>
2023-08-19 18:23:01 +0200

    DI example

---
+++ MinimalApis/Program.cs
@@ -0,0 +1,30 @@
+using MinimalApis.Baseline;
+using MinimalApis.Simple;
+using MinimalApis.WithDI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddTransient<ICalculator, Calculator>();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.AddWeatherForecastEndpoints();
+app.AddSimpleEndPoints();
+app.AddDependencyInjectionRelatedEndPoints();
+
+app.Run();
+
+
--- MinimalApis/WithDI/AdditionWithDependencyInjection.cs
+++ MinimalApis/WithDI/AdditionWithDependencyInjection.cs
@@ -1,5 +1,6 @@
 
 
+using Microsoft.AspNetCore.Mvc;
 using MinimalApis.Simple;
 
 namespace MinimalApis.WithDI;
@@ -9,7 +10,7 @@ public static class AdditionWithDependencyInjection
 
     public static WebApplication AddDependencyInjectionRelatedEndPoints(this WebApplication app)
     {
-        app.MapGet("/DI/Add", (int number1, int number2, ICalculator calculator) => 
+        app.MapGet("/DI/Add", (int number1, int number2, [FromServices] ICalculator calculator) => 
             calculator.Add(number1, number2));
         return app;
     }
