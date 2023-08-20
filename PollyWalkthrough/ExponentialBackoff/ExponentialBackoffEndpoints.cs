3f96d538f4ad255b76c0fc2717644555a41cfbf0
ThatStatsGuy <chris.dunderdale.1995@gmail.com>
2023-08-20 20:08:07 +0200

    backoff methods

--- PollyWalkthrough/ExponentialBackoff/ExponentialBackoffEndpoints.cs
+++ PollyWalkthrough/ExponentialBackoff/ExponentialBackoffEndpoints.cs
@@ -1,13 +1,29 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
 
 namespace PollyWalkthrough.ExponentialBackoff;
 
 public static class ExponentialBackoffEndpoints
 {
+
+    private static readonly IAsyncPolicy<HttpResponseMessage> _retryPolicy =
+        Policy<HttpResponseMessage>
+            .Handle<HttpRequestException>()
+            .OrResult(response =>
+                response.StatusCode is >= HttpStatusCode.InternalServerError or HttpStatusCode.RequestTimeout)
+            .WaitAndRetryAsync(Backoff.ExponentialBackoff(TimeSpan.FromSeconds(1), 5));
     public static WebApplication AddExponentialBackoffEndpoints(this WebApplication app)
     {
-        app.MapGet("/ExponentialBackoff", ([FromServices] IHttpClientFactory clientFactory) =>
+        app.MapGet("/ExponentialBackoff", async ([FromServices] IHttpClientFactory clientFactory) =>
         {
+            var client = clientFactory.CreateClient("boredapi");
+            var response = 
+                await _retryPolicy
+                    .ExecuteAsync(() => client.GetAsync("activity"));
+            return response.Content.ReadAsStringAsync();
+
 
         });
             
--- PollyWalkthrough/ExponentialBackoffWithJitter/ExponentialBackoffEndpointsWithJitter.cs
+++ PollyWalkthrough/ExponentialBackoffWithJitter/ExponentialBackoffEndpointsWithJitter.cs
@@ -1,13 +1,28 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
 
 namespace PollyWalkthrough.ExponentialBackoffWithJitter;
 
 public static class ExponentialBackoffEndpointsWithJitter
 {
+    private static readonly IAsyncPolicy<HttpResponseMessage> _retryPolicy =
+        Policy<HttpResponseMessage>
+            .Handle<HttpRequestException>()
+            .OrResult(response =>
+                response.StatusCode is >= HttpStatusCode.InternalServerError or HttpStatusCode.RequestTimeout)
+            .WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(1), 5));
+
     public static WebApplication AddExponentialBackoffWithJitterEndpoints(this WebApplication app)
     {
-        app.MapGet("/ExponentialBackoffWithJitter", ([FromServices] IHttpClientFactory clientFactory) =>
+        app.MapGet("/ExponentialBackoffWithJitter", async ([FromServices] IHttpClientFactory clientFactory) =>
         {
+            var client = clientFactory.CreateClient("boredapi");
+            var response = 
+                await _retryPolicy
+                    .ExecuteAsync(() => client.GetAsync("activity"));
+            return response.Content.ReadAsStringAsync();
 
         });
             
