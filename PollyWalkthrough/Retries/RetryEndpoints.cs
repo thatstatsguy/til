d8743e19db93cdede870ba3347c3d281ca54ba43
ThatStatsGuy <chris.dunderdale.1995@gmail.com>
2023-08-20 17:05:09 +0200

    add retry endpoint

--- PollyWalkthrough/Retries/RetryEndpoints.cs
+++ PollyWalkthrough/Retries/RetryEndpoints.cs
@@ -1,13 +1,28 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Polly;
 
 namespace PollyWalkthrough.Retries;
 
 public static class RetryEndpoints
 {
+    private static readonly IAsyncPolicy<HttpResponseMessage> _retryPolicy =
+        Policy<HttpResponseMessage>
+            .Handle<HttpRequestException>()
+            .OrResult(response =>
+                response.StatusCode is >= HttpStatusCode.InternalServerError or HttpStatusCode.RequestTimeout)
+            //retry up to 5 times
+            .RetryAsync(5);
+
     public static WebApplication AddRetryEndpoints(this WebApplication app)
     {
-        app.MapGet("/Retry", ([FromServices] IHttpClientFactory clientFactory) =>
+        app.MapGet("/Retry", async ([FromServices] IHttpClientFactory clientFactory) =>
         {
+            var client = clientFactory.CreateClient("boredapi");
+            var response = 
+                await _retryPolicy
+                    .ExecuteAsync(() => client.GetAsync("activity"));
+            return response.Content.ReadAsStringAsync();
 
         });
             
